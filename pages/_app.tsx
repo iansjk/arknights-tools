a2ed338d50e6e2bb149c1d4b69ba48b41798bab6
Ian Kim <iansjk@gmail.com>
2022-01-03 22:57:12 -0500

    Add MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+import "../styles/globals.css";
+
+const theme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
