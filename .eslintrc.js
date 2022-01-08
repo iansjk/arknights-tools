798c7f7f86a5af5bb1273006c2f3ae3756460884
Ian Kim <iansjk@gmail.com>
2022-01-08 02:48:32 -0500

    Add prettier as an eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
     "plugin:import/typescript",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
