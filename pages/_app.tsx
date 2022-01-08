0f82d5e67a26f35e9d8c591f37174e38fcf4b5d4
Ian Kim <iansjk@gmail.com>
2022-01-08 14:02:24 -0500

    Add siteMetadata to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,40 @@
-import type { AppProps } from "next/app";
 import { createTheme, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
 const theme = createTheme();
+
+export const siteMetadata = {
+  siteTitle: "Arknights Tools",
+  siteUrl: "https://samidare.io/arknights",
+  description:
+    "A collection of tools for Arknights, a tower defense mobile game by Hypergryph/Yostar",
+  pages: [
+    {
+      slug: "/planner",
+      pageTitle: "Operator Planner",
+      description: "Track material requirements for your operator goals",
+    },
+    {
+      slug: "/recruitment",
+      pageTitle: "Recruitment Calculator",
+      description:
+        "See what operators can be obtained from your recruitment tags",
+    },
+    {
+      slug: "/gacha",
+      pageTitle: "Pull Probability Calculator",
+      description:
+        "Calculate probabilities for obtaining units from a gacha banner",
+    },
+    {
+      slug: "/leveling",
+      pageTitle: "Leveling Costs",
+      description: "Show LMD and XP costs for leveling operators",
+    },
+  ],
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
