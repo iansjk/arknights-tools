48a752a47d471369220058610bb47ab47266f479
Ian Kim <iansjk@gmail.com>
2022-01-16 18:10:31 -0500

    Add styling/portraits for OperatorPlannerGoalCard

--- components/OperatorPlannerGoalCard.tsx
+++ components/OperatorPlannerGoalCard.tsx
@@ -1,39 +1,81 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, styled, Typography } from "@mui/material";
+import Image from "next/image";
 
 import operators from "../data/operators.json";
 import * as Output from "../scripts/output-types";
 
 import { OperatorPlannerGoal } from "./planner-types";
 
+const OperatorGoalList = styled("ul")(({ theme }) => ({
+  margin: theme.spacing(0, 0, 0, 1.5),
+  padding: 0,
+  listStyleType: "'- '",
+}));
+
+const OperatorGoalListItem = styled("li")(({ theme }) => ({}));
+
+const getPortraitSrc = (operator: Output.Operator, elite: number) => {
+  let filename = `${operator.id}_${elite === 2 ? 2 : 1}.png`;
+  if (operator.id === "char_002_amiya" && elite === 1) {
+    filename = "char_002_amiya_1+.png";
+  }
+  return `/images/portraits/${filename}`;
+};
+
 const OperatorPlannerGoalCard: React.VFC<OperatorPlannerGoal> = (props) => {
   const { operatorId, elite, skillLevel, masteries } = props;
   const operator: Output.Operator =
     operators[operatorId as keyof typeof operators];
   return (
-    <Card>
-      <CardContent>
-        <Typography component="h4" variant="h6">
+    <Card
+      component="li"
+      sx={{
+        position: "relative",
+      }}
+    >
+      <Box
+        position="absolute"
+        left={-40}
+        top={-40}
+        sx={{
+          "& img": {
+            maskImage:
+              "linear-gradient(to right, black, black 40%, transparent 80%)",
+          },
+        }}
+      >
+        <Image
+          src={getPortraitSrc(operator, elite?.end ?? 0)}
+          width={130}
+          height={260}
+          objectFit="contain"
+          objectPosition="bottom"
+          alt=""
+        />
+      </Box>
+      <CardContent sx={{ pl: 7, position: "relative", zIndex: 1 }}>
+        <Typography component="h4" variant="h6" gutterBottom>
           {operator.name}
         </Typography>
-        <ul>
+        <OperatorGoalList>
           {elite && (
-            <li>
+            <OperatorGoalListItem>
               Elite {elite.start} to Elite {elite.end}
-            </li>
+            </OperatorGoalListItem>
           )}
           {skillLevel && (
-            <li>
+            <OperatorGoalListItem>
               Skill Level {skillLevel.start} to Skill Level {skillLevel.end}
-            </li>
+            </OperatorGoalListItem>
           )}
           {masteries &&
             Object.entries(masteries).map(([skillNumber, range]) => (
-              <li key={skillNumber}>
+              <OperatorGoalListItem key={skillNumber}>
                 Skill {skillNumber}: Mastery {range.start} to Mastery{" "}
                 {range.end}
-              </li>
+              </OperatorGoalListItem>
             ))}
-        </ul>
+        </OperatorGoalList>
       </CardContent>
     </Card>
   );
