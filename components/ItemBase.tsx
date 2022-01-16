be8e7b8cf0037bff678625f17fe2c0cbd56ab797
Ian Kim <iansjk@gmail.com>
2022-01-16 03:23:35 -0500

    Reimplement ItemBase with next/image and MUI5

---
+++ components/ItemBase.tsx
@@ -0,0 +1,42 @@
+import { Box } from "@mui/material";
+import Image from "next/image";
+
+import items from "../data/items.json";
+import * as Output from "../scripts/output-types";
+
+const DEFAULT_SIZE = 100;
+
+interface ItemBaseProps {
+  id: string;
+  size?: number;
+}
+
+const ItemBase: React.VFC<ItemBaseProps> = ({ id, size = DEFAULT_SIZE }) => {
+  const item: Output.Item = items[id as keyof typeof items];
+  const bgSize = Math.floor(size * (95 / 100));
+  return (
+    <Box
+      sx={{
+        display: "inline-grid",
+        "& > *": {
+          gridArea: "1 / -1",
+        },
+      }}
+    >
+      <Image
+        src={`/images/items/itembg-tier-${item.tier}.png`}
+        width={bgSize}
+        height={bgSize}
+        alt=""
+      />
+      <Image
+        src={`/images/items/${item.iconId}.png`}
+        alt={item.name}
+        width={size}
+        height={size}
+        objectFit="contain"
+      />
+    </Box>
+  );
+};
+export default ItemBase;
