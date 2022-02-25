946bd0d754a3de157820256c0f7e2f443a233e7f
Ian Kim <iansjk@gmail.com>
2022-02-24 23:21:48 -0500

    Add SSR support for MUI v5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
+import { EmotionCache } from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import createEmotionCache from "../src/createEmotionCache";
 
 export const theme = createTheme({
   palette: {
     mode: "dark",
   },
 });
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
 export const siteMetadata = {
   siteTitle: "Arknights Tools",
@@ -34,13 +46,17 @@ export const siteMetadata = {
   },
 };
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
-
-export default MyApp;
