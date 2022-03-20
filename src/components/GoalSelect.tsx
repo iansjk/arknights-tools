6ed5c5fdc89737abac7df19708b6f7e1953a3467
Ian Kim <iansjk@gmail.com>
2022-03-20 17:28:06 -0400

    Implement GoalSelect presets

--- src/components/GoalSelect.tsx
+++ src/components/GoalSelect.tsx
@@ -1,5 +1,6 @@
 import {
   Checkbox,
+  Divider,
   FormControl,
   InputLabel,
   ListItemText,
@@ -13,9 +14,26 @@ import { useEffect, useState } from "react";
 
 import { Operator } from "../../scripts/output-types";
 
-const GoalMenuItem = styled(MenuItem)`
-  pr: "15px";
-`;
+const PRESETS = new Set([
+  "Elite 1 Skill Level 7",
+  "Skill 1 Mastery 1 → 3",
+  "Skill 2 Mastery 1 → 3",
+  "Skill 3 Mastery 1 → 3",
+  "Everything",
+]);
+
+const GoalMenuCheckboxItem = styled(MenuItem)(({ theme }) => ({
+  height: "50px",
+  "& .MuiCheckbox-root": {
+    marginLeft: theme.spacing(-1),
+    marginRight: theme.spacing(0.5),
+  },
+}));
+
+const GoalMenuPlainItem = styled(MenuItem)(() => ({
+  paddingLeft: "25px",
+  height: "50px",
+}));
 
 interface Props {
   operator: Operator | null;
@@ -30,9 +48,72 @@ const GoalSelect: React.VFC<Props> = (props) => {
   }, [operator]);
 
   const handleChange = (e: SelectChangeEvent<string[]>) => {
-    setGoalNames(
-      typeof e.target.value === "string" ? [e.target.value] : e.target.value
-    );
+    const newGoalNames =
+      typeof e.target.value === "string" ? [e.target.value] : e.target.value;
+    const newSpecificGoals = new Set<string>();
+    const newPresets = [];
+    for (const goalName of newGoalNames) {
+      if (PRESETS.has(goalName)) {
+        newPresets.push(goalName);
+      } else {
+        newSpecificGoals.add(goalName);
+      }
+    }
+    for (const preset of newPresets) {
+      switch (preset) {
+        case "Elite 1 Skill Level 7":
+          newSpecificGoals.add("Elite 1");
+          newSpecificGoals.add("Skill Level 2");
+          newSpecificGoals.add("Skill Level 3");
+          newSpecificGoals.add("Skill Level 4");
+          newSpecificGoals.add("Skill Level 5");
+          newSpecificGoals.add("Skill Level 6");
+          newSpecificGoals.add("Skill Level 7");
+          break;
+        case "Skill 1 Mastery 1 → 3":
+          newSpecificGoals.add("Skill 1 Mastery 1");
+          newSpecificGoals.add("Skill 1 Mastery 2");
+          newSpecificGoals.add("Skill 1 Mastery 3");
+          break;
+        case "Skill 2 Mastery 1 → 3":
+          newSpecificGoals.add("Skill 2 Mastery 1");
+          newSpecificGoals.add("Skill 2 Mastery 2");
+          newSpecificGoals.add("Skill 2 Mastery 3");
+          break;
+        case "Skill 3 Mastery 1 → 3":
+          newSpecificGoals.add("Skill 3 Mastery 1");
+          newSpecificGoals.add("Skill 3 Mastery 2");
+          newSpecificGoals.add("Skill 3 Mastery 3");
+          break;
+        case "Everything": {
+          if (operator!.module != null) {
+            newSpecificGoals.add("Module");
+          }
+          newSpecificGoals.add("Elite 1");
+          newSpecificGoals.add("Elite 2");
+          newSpecificGoals.add("Skill Level 2");
+          newSpecificGoals.add("Skill Level 3");
+          newSpecificGoals.add("Skill Level 4");
+          newSpecificGoals.add("Skill Level 5");
+          newSpecificGoals.add("Skill Level 6");
+          newSpecificGoals.add("Skill Level 7");
+          newSpecificGoals.add("Skill 1 Mastery 1");
+          newSpecificGoals.add("Skill 1 Mastery 2");
+          newSpecificGoals.add("Skill 1 Mastery 3");
+          newSpecificGoals.add("Skill 2 Mastery 1");
+          newSpecificGoals.add("Skill 2 Mastery 2");
+          newSpecificGoals.add("Skill 2 Mastery 3");
+          newSpecificGoals.add("Skill 3 Mastery 1");
+          newSpecificGoals.add("Skill 3 Mastery 2");
+          newSpecificGoals.add("Skill 3 Mastery 3");
+          break;
+        }
+        default:
+          console.warn("Unknown preset: ", preset);
+          break;
+      }
+    }
+    setGoalNames([...newSpecificGoals]);
   };
 
   const renderOptions = () => {
@@ -42,68 +123,90 @@ const GoalSelect: React.VFC<Props> = (props) => {
 
     const elite =
       operator.elite.length > 0
-        ? [
-            <ListSubheader key="elite">Elite</ListSubheader>,
-            ...operator.elite.map((goal) => (
-              <GoalMenuItem key={goal.name} value={goal.name}>
-                <Checkbox
-                  checked={goalNames.indexOf(goal.name) > -1}
-                  size="small"
-                />
-                <ListItemText primary={goal.name} />
-              </GoalMenuItem>
-            )),
-          ]
+        ? operator.elite.map((goal) => (
+            <GoalMenuCheckboxItem key={goal.name} value={goal.name}>
+              <Checkbox
+                checked={goalNames.indexOf(goal.name) > -1}
+                size="small"
+              />
+              <ListItemText primary={goal.name} />
+            </GoalMenuCheckboxItem>
+          ))
         : [];
     const skillLevel =
       operator.skillLevels.length > 0
-        ? [
-            <ListSubheader key="skillLevels">Skill Levels</ListSubheader>,
-            ...operator.skillLevels.map((goal) => (
-              <GoalMenuItem key={goal.name} value={goal.name}>
-                <Checkbox
-                  checked={goalNames.indexOf(goal.name) > -1}
-                  size="small"
-                />
-                <ListItemText primary={goal.name} />
-              </GoalMenuItem>
-            )),
-          ]
+        ? operator.skillLevels.map((goal) => (
+            <GoalMenuCheckboxItem key={goal.name} value={goal.name}>
+              <Checkbox
+                checked={goalNames.indexOf(goal.name) > -1}
+                size="small"
+              />
+              <ListItemText primary={goal.name} />
+            </GoalMenuCheckboxItem>
+          ))
         : [];
     const masteryGoals = operator.skills.flatMap((sk) => sk.masteries);
     const mastery =
       masteryGoals.length > 0
-        ? [
-            <ListSubheader key="masteries">Masteries</ListSubheader>,
-            ...masteryGoals.map((goal) => (
-              <GoalMenuItem key={goal.name} value={goal.name}>
-                <Checkbox
-                  checked={goalNames.indexOf(goal.name) > -1}
-                  size="small"
-                />
-                <ListItemText primary={goal.name} />
-              </GoalMenuItem>
-            )),
-          ]
+        ? masteryGoals.map((goal) => (
+            <GoalMenuCheckboxItem key={goal.name} value={goal.name}>
+              <Checkbox
+                checked={goalNames.indexOf(goal.name) > -1}
+                size="small"
+              />
+              <ListItemText primary={goal.name} />
+            </GoalMenuCheckboxItem>
+          ))
         : [];
 
     const module =
       operator.module != null ? (
-        <GoalMenuItem
-          key={operator.module.name}
-          value="Module"
-          sx={{
-            pl: "25px",
-            height: "50px",
-          }}
-        >
+        <GoalMenuPlainItem key={operator.module.name} value="Module">
           Module
-        </GoalMenuItem>
+        </GoalMenuPlainItem>
       ) : null;
 
+    const presets = [
+      <ListSubheader key="presets">Presets</ListSubheader>,
+      <GoalMenuPlainItem
+        key="Elite 1 Skill Level 7"
+        value="Elite 1 Skill Level 7"
+      >
+        Elite 1 Skill Level 7
+      </GoalMenuPlainItem>,
+      ...operator.skills.map((_, i) => {
+        const masteryPresetName = `Skill ${i + 1} Mastery 1 → 3`;
+        return (
+          <GoalMenuPlainItem key={masteryPresetName} value={masteryPresetName}>
+            {masteryPresetName}
+          </GoalMenuPlainItem>
+        );
+      }),
+      <GoalMenuPlainItem key="Everything" value="Everything">
+        Everything
+      </GoalMenuPlainItem>,
+      <ListSubheader key="goals">Goals</ListSubheader>,
+    ];
+
     return module != null
-      ? [module, ...elite, ...skillLevel, ...mastery]
-      : [...elite, ...skillLevel, ...mastery];
+      ? [
+          ...presets,
+          module,
+          <Divider key="1" />,
+          ...elite,
+          <Divider key="2" />,
+          ...skillLevel,
+          <Divider key="3" />,
+          ...mastery,
+        ]
+      : [
+          ...presets,
+          elite,
+          <Divider key="4" />,
+          ...skillLevel,
+          <Divider key="5" />,
+          ...mastery,
+        ];
   };
 
   return (
