85e32f7a7175b3b45feed244c5cf89820d1447cd
Ian Kim <me@ian.kim>
2022-04-03 18:42:22 -0400

    Virtualize OperatorSearch results

--- src/components/OperatorSearch.tsx
+++ src/components/OperatorSearch.tsx
@@ -1,7 +1,8 @@
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Autocomplete, Box, InputAdornment, TextField } from "@mui/material";
+import { Autocomplete, autocompleteClasses, Box, InputAdornment, Popper, styled, TextField, Typography} from "@mui/material";
 import Image from "next/image";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
+import { VariableSizeList, ListChildComponentProps } from 'react-window';
 
 import operatorsJson from "../../data/operators.json";
 import { Operator } from "../../scripts/output-types";
@@ -27,6 +28,8 @@ const OperatorSearch: React.VFC<Props> = (props) => {
       autoHighlight
       value={value}
       onChange={(_: unknown, newValue: Operator | null) => onChange(newValue)}
+      PopperComponent={StyledPopper}
+      ListboxComponent={ListboxComponent}
       options={operators}
       getOptionLabel={(op) => op.name}
       renderInput={(params) => {
@@ -62,20 +65,8 @@ const OperatorSearch: React.VFC<Props> = (props) => {
           />
         );
       }}
-      renderOption={(props, option) => (
-        <li {...props}>
-          <Box mr={2} display="inline-flex" alignItems="center">
-            <Image
-              src={`/arknights/avatars/${option.id}`}
-              width={32}
-              height={32}
-              alt=""
-              className="operator-avatar"
-            />
-          </Box>
-          {option.name}
-        </li>
-      )}
+      renderOption={(props, option) => [props, option]}
+      disableListWrap
       filterOptions={(options, state) => {
         return options.filter((op) =>
           op.name
@@ -91,3 +82,105 @@ const OperatorSearch: React.VFC<Props> = (props) => {
   );
 };
 export default OperatorSearch;
+
+// virtualization behavior from this example:
+// https://mui.com/components/autocomplete/#virtualization
+const StyledPopper = styled(Popper)({
+  [`& .${autocompleteClasses.listbox}`]: {
+    boxSizing: 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
+
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props: ListChildComponentProps) {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  const inlineStyle = {
+    ...style,
+    top: (style.top as number) + LISTBOX_PADDING,
+  };
+
+  return (
+    <Typography component="li" {...dataSet[0]} noWrap style={inlineStyle}>
+      <Box mr={2} display="inline-flex" alignItems="center">
+        <Image
+          src={`/arknights/avatars/${dataSet[1].id}`}
+          width={32}
+          height={32}
+          alt=""
+          className="operator-avatar"
+        />
+      </Box>
+      {dataSet[1].name}
+    </Typography>
+  );
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data: any) {
+  const ref = React.useRef<VariableSizeList>(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLElement>
+>(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData: React.ReactChild[] = [];
+  (children as React.ReactChild[]).forEach(
+    (item: React.ReactChild & { children?: React.ReactChild[] }) => {
+      itemData.push(item);
+      itemData.push(...(item.children || []));
+    },
+  );
+
+  const itemCount = itemData.length;
+  const itemSize = 44;
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.length * itemSize;
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={() => itemSize}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
