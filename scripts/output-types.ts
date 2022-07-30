91483ecf432e8db2ce1c7afc43e6f963b23bf6eb
Ian Kim <iansjk@gmail.com>
2022-07-29 20:00:32 -0400

    Fix typo in modules property

--- scripts/output-types.ts
+++ scripts/output-types.ts
@@ -87,7 +87,7 @@ export interface Operator {
   skillLevels: SkillLevelGoal[];
   elite: EliteGoal[];
   skills: Skill[];
-  module?: Module[];
+  modules?: Module[];
 }
 
 export interface RecruitableOperator {
